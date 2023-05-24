b8daf8ee1f8ba9b787fb649a774a89c66ec9d474
Heck21 <cavinawe12@gmail.com>
2023-05-24 03:56:40 -0500

    Changed visuals.

--- sparkcalculator.py
+++ sparkcalculator.py
@@ -18,49 +18,54 @@ def calculate_spark() -> None:
         percentage_label.config(text=f"Percentage: {percentage:.2f}%")
 
 
+bg_color = "white"
+
 root = tk.Tk()
 root.title("Spark Calculator")
 root.geometry("340x360")
+root.configure(background=bg_color)
 root.resizable(False, False)
 
-padding = tk.Label(root, height=2)
+padding = tk.Label(root, height=2, bg=bg_color)
 padding.pack()
 
-single_draw_label = tk.Label(root, text="Single Draw Tickets:")
+single_draw_label = tk.Label(root, text="Single Draw Tickets:", bg=bg_color)
 single_draw_label.pack()
-single_draw_entry = tk.Entry(root, justify="center")
+single_draw_entry = tk.Entry(root, justify="center", bg=bg_color)
 single_draw_entry.insert(0, "0")
 single_draw_entry.pack()
 
-ten_draw_label = tk.Label(root, text="Ten Draw Tickets:")
+ten_draw_label = tk.Label(root, text="Ten Draw Tickets:", bg=bg_color)
 ten_draw_label.pack()
-ten_draw_entry = tk.Entry(root, justify="center")
+ten_draw_entry = tk.Entry(root, justify="center", bg=bg_color)
 ten_draw_entry.insert(0, "0")
 ten_draw_entry.pack()
 
-crystals_label = tk.Label(root, text="Crystals:")
+crystals_label = tk.Label(root, text="Crystals:", bg=bg_color)
 crystals_label.pack()
-crystals_entry = tk.Entry(root, justify="center")
+crystals_entry = tk.Entry(root, justify="center", bg=bg_color)
 crystals_entry.insert(0, "0")
 crystals_entry.pack()
 
-padding = tk.Label(root, height=2)
+padding = tk.Label(root, height=2, bg=bg_color)
 padding.pack()
 
-calculate_button = tk.Button(root, text="Calculate", command=calculate_spark)
+calculate_button = tk.Button(
+    root, text="Calculate", command=calculate_spark, bg=bg_color
+)
 calculate_button.pack()
 
-padding = tk.Label(root, height=2)
+padding = tk.Label(root, height=2, bg=bg_color)
 padding.pack()
 
-rolls_label = tk.Label(root, text="Total Rolls: ")
+rolls_label = tk.Label(root, text="Total Rolls: ", bg=bg_color)
 rolls_label.pack()
-sparks_label = tk.Label(root, text="Sparks: ")
+sparks_label = tk.Label(root, text="Sparks: ", bg=bg_color)
 sparks_label.pack()
-percentage_label = tk.Label(root, text="Percentage: ")
+percentage_label = tk.Label(root, text="Percentage: ", bg=bg_color)
 percentage_label.pack()
 
-padding = tk.Label(root, height=2)
+padding = tk.Label(root, height=3, bg=bg_color)
 padding.pack()
 
 root.mainloop()
